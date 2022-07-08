13815339822b435a47444bc29c5f72b5ce7c0c80
mmulika <mulikamike@gmail.com>
2022-07-08 16:41:40 +0300

    test

---
+++ Welcome/personality.py
@@ -0,0 +1,13 @@
+books=int(input("enter number of books purchase:"))
+
+if books==0:
+    print("point awarded:0")
+if books==1:
+    print("points awarded:6")
+if books==2:
+    print("points awarded:16")
+if books==3:
+    print("points awarded:32")
+
+else:
+    print("out of range")
