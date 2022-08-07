7f68cc5352ca2e8e0344a89acf5c346ce69def7d
mmulika <mulikamike@gmail.com>
2022-08-06 22:12:24 -0700

    member

--- day3/calories.py
+++ day3/calories.py
@@ -8,3 +8,26 @@ total_calories= (fat_calories + carbo_calories)
 print("Fat Calories = " ,fat_calories)
 print("Carbohydrate Calories = " , carbo_calories )
 print("Total Calories = " , total_calories )
+
+print('LA')
+
+def mass():{
+    print("hello")
+}
+
+
+class Employee():
+    def __init__(self,name, email ,dependants): 
+        self.name = name 
+        self.email = email
+        self.dependants = dependants
+    def EmployeeModel(self):
+        
+        name =db.Colomn(db.Integer(400))
+        email = db.Column(db.Email)
+        dependants =db.Column(db.String)
+
+        return(name,email,dependants)
+
+        employee =Employee()
+
